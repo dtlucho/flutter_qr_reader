7c5ace5080cc3da6f9dfa90bff7611097c39cb7f
dtlucho <luciano.ditomaso@gmail.com>
2022-01-06 20:52:11 -0300

    main stucture

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_qr_reader/widgets/custom_bottom_navigator.dart';
+import 'package:flutter_qr_reader/widgets/scan_button.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,11 +8,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('History'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.delete_forever))
+        ],
+      ),
       body: Center(
         child: Container(
           child: Text('Hello world'),
         ),
       ),
+      bottomNavigationBar: CustomBottomNavigator(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
