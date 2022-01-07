bb04079ac65d10f98e8ed171ecdf74e10518067d
dtlucho <luciano.ditomaso@gmail.com>
2022-01-06 21:37:39 -0300

    using provider to change manu selected option

--- lib/widgets/custom_bottom_navigator.dart
+++ lib/widgets/custom_bottom_navigator.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_qr_reader/providers/ui_provider.dart';
+import 'package:provider/provider.dart';
 
 class CustomBottomNavigator extends StatelessWidget {
   const CustomBottomNavigator({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final UiProvider uiProvider = Provider.of<UiProvider>(context);
+    final int currentIndex = uiProvider.selectedMenuOption;
+
     return BottomNavigationBar(
       elevation: 0,
-      currentIndex: 0,
+      currentIndex: currentIndex,
+      onTap: (index) => uiProvider.selectedMenuOption = index,
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.map),
