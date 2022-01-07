b284378bde37e6611ef6e8707af8a3a9b13c88f2
dtlucho <luciano.ditomaso@gmail.com>
2022-01-06 21:08:07 -0300

    fix in naming

--- lib/pages/urls_history_page.dart
+++ lib/pages/urls_history_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MapsHistoryPage extends StatelessWidget {
-  const MapsHistoryPage({Key? key}) : super(key: key);
+class UrlsHistoryPage extends StatelessWidget {
+  const UrlsHistoryPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
