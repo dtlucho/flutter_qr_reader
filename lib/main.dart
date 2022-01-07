d25fb6b0937722d33ee9ffb99e591f5af5df3592
dtlucho <luciano.ditomaso@gmail.com>
2022-01-06 21:14:16 -0300

    adding constructor

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_qr_reader/pages/home_page.dart';
 import 'package:flutter_qr_reader/pages/map_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
