cf3e54c33f92a58015d59253b29eda37a634c9bc
dtlucho <luciano.ditomaso@gmail.com>
2022-01-15 20:22:56 -0300

    implementing scan list provider to load scans by tipe

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_qr_reader/pages/maps_history_page.dart';
 import 'package:flutter_qr_reader/pages/urls_history_page.dart';
-import 'package:flutter_qr_reader/providers/db_provider.dart';
+import 'package:flutter_qr_reader/providers/scan_list_provider.dart';
 import 'package:flutter_qr_reader/providers/ui_provider.dart';
 import 'package:flutter_qr_reader/widgets/custom_bottom_navigator.dart';
 import 'package:flutter_qr_reader/widgets/scan_button.dart';
@@ -37,12 +37,17 @@ class _HomePageBody extends StatelessWidget {
     final UiProvider uiProvider = Provider.of<UiProvider>(context);
     final int currentIndex = uiProvider.selectedMenuOption;
 
-    DBProvider.db.database;
+    final ScanListProvider scanListProvider = Provider.of<ScanListProvider>(
+      context,
+      listen: false,
+    );
 
     switch (currentIndex) {
       case 0:
+        scanListProvider.loadScansByType("geo");
         return const MapsHistoryPage();
       case 1:
+        scanListProvider.loadScansByType("http");
         return const UrlsHistoryPage();
       default:
         return const MapsHistoryPage();
