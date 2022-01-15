e96f4ebd4941747375432dc2060c8231adf228a1
dtlucho <luciano.ditomaso@gmail.com>
2022-01-15 19:35:12 -0300

    adding a scan to the list and inserting it to the db

--- lib/providers/scan_list_provider.dart
+++ lib/providers/scan_list_provider.dart
@@ -4,4 +4,15 @@ import 'package:flutter_qr_reader/providers/db_provider.dart';
 class ScanListProvider extends ChangeNotifier {
   List<ScanModel> scans = [];
   String selectedType = "http";
+
+  void newScan(String value) async {
+    final newScan = ScanModel(value: value);
+    final id = await DBProvider.db.newScan(newScan);
+    newScan.id = id;
+
+    if (selectedType == newScan.type) {
+      scans.add(newScan);
+      notifyListeners();
+    }
+  }
 }
