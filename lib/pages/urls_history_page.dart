8ac41c8efe9f69f982d6b49cecd2e7eb794c1c1e
dtlucho <luciano.ditomaso@gmail.com>
2022-01-15 20:27:29 -0300

    urls history page layout + scan list provider

--- lib/pages/urls_history_page.dart
+++ lib/pages/urls_history_page.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_qr_reader/models/scan_model.dart';
+import 'package:flutter_qr_reader/providers/scan_list_provider.dart';
+import 'package:provider/provider.dart';
 
 class UrlsHistoryPage extends StatelessWidget {
   const UrlsHistoryPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Urls history page'),
+    final ScanListProvider scanListProvider =
+        Provider.of<ScanListProvider>(context);
+
+    final List<ScanModel> scans = scanListProvider.scans;
+
+    return ListView.builder(
+      itemCount: scans.length,
+      itemBuilder: (_, int index) {
+        return ListTile(
+          leading: Icon(Icons.link, color: Theme.of(context).primaryColor),
+          title: Text(scans[index].value),
+          subtitle: Text(scans[index].id.toString()),
+          trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+          onTap: () => print(scans[index].id),
+        );
+      },
     );
   }
 }
