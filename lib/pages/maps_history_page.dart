e0e8364f9c74560980471e1795ca91b090443a0e
dtlucho <luciano.ditomaso@gmail.com>
2022-01-15 20:52:43 -0300

    using scan list widget

--- lib/pages/maps_history_page.dart
+++ lib/pages/maps_history_page.dart
@@ -1,39 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_qr_reader/models/scan_model.dart';
-import 'package:flutter_qr_reader/providers/scan_list_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_qr_reader/widgets/scan_list.dart';
 
 class MapsHistoryPage extends StatelessWidget {
   const MapsHistoryPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final ScanListProvider scanListProvider =
-        Provider.of<ScanListProvider>(context);
-
-    final List<ScanModel> scans = scanListProvider.scans;
-
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder: (_, int index) {
-        return Dismissible(
-          key: UniqueKey(),
-          background: Container(color: Colors.red),
-          direction: DismissDirection.endToStart,
-          onDismissed: (direction) {
-            Provider.of<ScanListProvider>(context, listen: false)
-                .deleteScanById(scans[index].id!);
-          },
-          child: ListTile(
-            leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
-            title: Text(scans[index].value),
-            subtitle: Text(scans[index].id.toString()),
-            trailing:
-                const Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-            onTap: () => print(scans[index].id),
-          ),
-        );
-      },
-    );
+    return const ScanList(type: "geo");
   }
 }
--- lib/pages/urls_history_page.dart
+++ lib/pages/urls_history_page.dart
@@ -1,29 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_qr_reader/models/scan_model.dart';
-import 'package:flutter_qr_reader/providers/scan_list_provider.dart';
-import 'package:provider/provider.dart';
+
+import 'package:flutter_qr_reader/widgets/scan_list.dart';
 
 class UrlsHistoryPage extends StatelessWidget {
   const UrlsHistoryPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final ScanListProvider scanListProvider =
-        Provider.of<ScanListProvider>(context);
-
-    final List<ScanModel> scans = scanListProvider.scans;
-
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder: (_, int index) {
-        return ListTile(
-          leading: Icon(Icons.link, color: Theme.of(context).primaryColor),
-          title: Text(scans[index].value),
-          subtitle: Text(scans[index].id.toString()),
-          trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-          onTap: () => print(scans[index].id),
-        );
-      },
-    );
+    return const ScanList(type: "http");
   }
 }
