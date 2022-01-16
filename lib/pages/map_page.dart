142bf78bbc72c91552146f98ca1b3fe82efe9022
dtlucho <luciano.ditomaso@gmail.com>
2022-01-16 19:36:41 -0300

    receiving a scan

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_qr_reader/models/scan_model.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final ScanModel scan =
+        ModalRoute.of(context)!.settings.arguments as ScanModel;
+
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Map'),
+      ),
       body: Center(
-        child: Text('Hello world'),
+        child: Text(scan.value),
       ),
     );
   }
