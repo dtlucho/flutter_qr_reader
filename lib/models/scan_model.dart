215819abeb56b0580d0d01dbb05ef17304cb1115
dtlucho <luciano.ditomaso@gmail.com>
2022-01-20 20:13:36 -0300

    getting latitude and longitude

--- lib/models/scan_model.dart
+++ lib/models/scan_model.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
+
 class ScanModel {
   ScanModel({
     this.id,
@@ -12,6 +14,15 @@ class ScanModel {
   int? id;
   String? type;
   String value;
+
+  LatLng getLatLng() {
+    final latLng = value.substring(4).split(',');
+
+    final lat = double.parse(latLng[0]);
+    final lng = double.parse(latLng[1]);
+
+    return LatLng(lat, lng);
+  }
 
   factory ScanModel.fromRawJson(String str) =>
       ScanModel.fromJson(json.decode(str));
